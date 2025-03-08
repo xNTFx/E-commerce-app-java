caac4b6dda4517d0193e4b0977f9b692880790b5
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2025-03-08 18:02:01 +0100

    Update CorsConfig.java

--- backend/src/main/java/config/CorsConfig.java
+++ backend/src/main/java/config/CorsConfig.java
@@ -1,24 +1,38 @@
-package com.e_commerce.E_commerce.App.config;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
+    private final Environment environment;
+
+    public CorsConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Apply CORS to all endpoints
-                        .allowedOriginPatterns("http://localhost:5173") // Explicitly allow frontend URL
+                String[] allowedOrigins;
+
+                if (environment.acceptsProfiles("development")) {
+                    allowedOrigins = new String[]{"http://localhost:5173"};
+                } else {
+                    allowedOrigins = new String[]{"https://api.e-commerce-app-java.pawelsobon.pl"};
+                }
+
+                registry.addMapping("/**")
+                        .allowedOriginPatterns(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
-                        .allowCredentials(true); // Allow credentials (cookies, auth tokens)
+                        .allowCredentials(true);
             }
         };
     }
