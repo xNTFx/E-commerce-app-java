c0804df409f32b7680e6c75378d3a4ba34ebc053
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2025-03-08 18:08:45 +0100

    Update CorsConfig.java

--- backend/src/main/java/config/CorsConfig.java
+++ backend/src/main/java/config/CorsConfig.java
@@ -1,34 +1,18 @@
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
-    private final Environment environment;
-
-    public CorsConfig(Environment environment) {
-        this.environment = environment;
-    }
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                String[] allowedOrigins;
-
-                if (environment.acceptsProfiles("development")) {
-                    allowedOrigins = new String[]{"http://localhost:5173"};
-                } else {
-                    allowedOrigins = new String[]{"https://api.e-commerce-app-java.pawelsobon.pl"};
-                }
-
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(allowedOrigins)
+                        .allowedOriginPatterns("https://api.e-commerce-app-java.pawelsobon.pl")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
